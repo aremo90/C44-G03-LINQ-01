c5c21d5f36dcf90fb383de8b96ea678245b26603
aremo90 <mrhamza2005@gmail.com>
2025-08-29 08:24:33 +0300

    00 Data Setup

--- Program.cs
+++ Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region LINQ - Restriction Operators
+
+
+
+            #endregion
+
         }
     }
 }
