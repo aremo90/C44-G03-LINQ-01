2897745c0b77687e59946c6e93710c964515319a
aremo90 <mrhamza2005@gmail.com>
2025-08-29 05:46:47 +0300

    01 Implicity type local variable

--- Program.cs
+++ Program.cs
@@ -4,7 +4,42 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region Implicity type local variable
+
+            #region VAR
+            ////c# 3.0
+            //var Name = "John";
+            //// Compiler Will detect the dataType of variable at compile time 
+
+            ////var x = null // Invaild
+
+            ////Name = 1; // invaild
+
+            //Name = null; // vaild
+
+            ////var age; // invaild
+
+            //// Cannot use keyword var in parmater OR Retun type OR arribute
+            //// we use it only for local variable 
+            ///
+            #endregion
+
+            #region DYNAMIC
+            //// c# 4.0
+            //// not stfe
+            //dynamic age = 30; // vaild  
+            //// CLR will Detect the dataType at run time
+
+            //age = "Thirty"; // vaild
+            //age = null; // vaild will trow exception at run time if we try to access it
+            //age = 10.5; // vaild
+
+            Console.WriteLine(age);
+            #endregion
+            #endregion
+
+
         }
     }
 }
